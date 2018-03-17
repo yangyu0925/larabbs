9df7cc7330235d88af1f093520e73f5a6f223604
yangyu <172964776@qq.com>
2018-03-17 12:05:37 +0800

    Category topic list

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,5 @@ $this->post('password/reset', 'Auth\ResetPasswordController@reset');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
